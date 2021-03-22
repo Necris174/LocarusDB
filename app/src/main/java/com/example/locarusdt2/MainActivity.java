6b245d848e7c18123a093fee5a02f6a24cee597d
Necris174 <plotnicovvyu@gmail.com>
2021-03-22 14:59:51 +0500

    Json2

--- app/src/main/java/com/example/locarusdt2/MainActivity.java
+++ app/src/main/java/com/example/locarusdt2/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,21 +66,48 @@ public class MainActivity extends AppCompatActivity {
         if (validateLogin() | validatePassword()) {
             return;
         }
+        getJSon();
+        Log.d("MyLogTag", "Ответ от сервера ");
     }
 
     private void getJSon (){
-        String url = "http://stage.local/api/";
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        String postUrl = "http://stage.local/api/";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        JSONObject login = new JSONObject();
+        try {
+            postData.put("jsonrpc", "2.0");
+            postData.put("method", "signin");
+            login.put("login", "admin");
+            login.put("password", "123456");
+            postData.put("params", login);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d("MyLogTag", "Ответ от сервера " + e);
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, postUrl, postData, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-
+                Log.d("MyLogTag", "Ответ от сервера " + response);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
+                Log.d("MyLogTag", "Ответ от сервера " + error);
             }
-        });
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Accept", "application/json");
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
 
     }
 }
